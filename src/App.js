ac94457edc7b25cb640db261c00ff754367e3cfe
Jason Lu <JJCY@Jasons-MacBook.broadband>
2020-06-27 16:58:41 +0100

    base of calculator

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import {useState} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  const [userInput, setUserInput] = useState('')
+  const onClickHandler = () => {
+    setUserInput(userInput + '7')
+  }
+
+  return <div className='container'>
+    <h1>Calculator</h1>
+<div className='userInput'>{userInput}</div>
+    <div className='box'>
+      <button onClick={onClickHandler}>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>+</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>-</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>*</button>
+      <button>.</button>
+      <button>0</button>
+      <button>=</button>
+      <button>/</button>
+            
     </div>
-  );
+  </div>
 }
 
-export default App;
+export default App
